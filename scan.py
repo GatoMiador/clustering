56ffcb2e5f41a3fd7b7180a5f60a45dba2525098
Marcos Vicente Cruz <marcoscruz@sigmasysengenharia.com.br>
2022-12-07 11:29:04 -0300

    Trocados os nomes de algumas variáveis para ficarem iguais ao artigo.

--- scan.py
+++ scan.py
@@ -13,22 +13,22 @@ def scan(path):
     import numpy as np
     import scipy.stats
 
-    # Variação mínima de potência a considerar
+    # Variação mínima de potência a considerar como arranque
     ΔP = 2.5
     # Quantidade de amostras por ciclo
     amostra_por_ciclo = 1024
     # Frequencia da rede
-    freq = 60
+    frequencia = 60
     # Tempo para considerar o sinal estável, isso remove o "spike" inicial de algumas cargas
     tempo_estabilização = amostra_por_ciclo*2
     # Ciclos a capturar antes do evento
     n_before = amostra_por_ciclo * 2
     # Ciclos a capturar depois do evento
-    n_after = 5 * freq * amostra_por_ciclo
+    n_after = 5 * frequencia * amostra_por_ciclo
     # Porcentagem da potência a manter para considerar que o circuito ativou
     p_min = 0.9
-    # Ciclos a ignorar no início
-    ign = (amostra_por_ciclo*2)-1
+    # Ciclos a ignorar no início por causa do calcula da CPT
+    ign = amostra_por_ciclo
 
     nomes = [ 'P', 'Q', 'D', 'V', 'I', 'Ia', 'Ir', 'Iv' ]
     filename = os.path.splitext(os.path.basename(path) )[0]
@@ -41,18 +41,17 @@ def scan(path):
                              usecols=nomes, \
                              chunksize=300*1024*1024):
         for n, amostra in chunk.iterrows():
-            if passo == 0: # Espera pela estabilização dos dados
+            if passo == 0: # Espera pela estabilização do calculo dos dados da CPT
                 if n >= ign:
                     # Guarda o patamar inicial                    
                     ac = amostra
                     passo = 1
-            elif passo == 1: # Verifica se a variação é maior que o threshold
+            elif passo == 1: # Verifica se a variação é maior que o threshold ΔP
                 d = abs(amostra['P'] - ac['P'])
                 if d >= ΔP:
                     n0 = n
                     passo = 2
             elif passo == 2: # Espera o delta se manter pelo tempo
-                # Só classifica se a corrente se mantém acima do delta pelo tempo
                 d = abs(amostra['P'] - ac['P'])
                 if d >= ΔP:
                     if (n - n0) >= tempo_estabilização:
@@ -60,24 +59,23 @@ def scan(path):
                         P1 = amostra['P']
                         n0 = n
                         # Salva o início do evento
-                        start_event = n - tempo_estabilização
+                        inicio_arranque = n - tempo_estabilização
                         passo = 3
                 else:
                     passo = 1 # Volta para o passo 1 se baixou
-            elif passo == 3: # Espera estabilizar
-                # Verifica se estabilizou
+            elif passo == 3: # Verifica se a medição caiu novamente
                 d = abs(amostra['P'] - P1)
                 if P0 > d:
                     if (n - n0) >= tempo_estabilização:
                         # Verifica se a carga entrou ou saiu
                         d = amostra['I'] - ac['I']
                         if d >= 0:
-                            ac['ini'] = start_event;
+                            ac['ini'] = inicio_arranque;
 
                             # Cria aqui o degrau para comparar
                             cmp = np.full(n_before, ac['P']).tolist()
                             cmp = cmp + np.full(n_after, amostra['P']).tolist()
-                            cut = chunk.P[start_event-n_before: start_event+n_after]
+                            cut = chunk.P[inicio_arranque-n_before: inicio_arranque+n_after]
                             # Corta o tamanho extra caso a amostra seja pequena demais
                             max = min(len(cmp), len(cut) )
                             cut = cut.iloc[0:max]
